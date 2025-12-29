62411f09eab253825896b05b9c048efb39faa2d4
Zara B <zara.b2000@example.com>
2025-12-29 23:13:25 +0530

    add loggings

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 const employeeRoutes = require("./routes/employee.routes");
 const errorHandler = require("./middlewares/errorHandler");
+const logger = require("./middlewares/logger");
 const app = express();
 connectDB();
 
 app.use(express.json());
+
+app.use(logger);
 
 app.use("/employees", employeeRoutes);
 
