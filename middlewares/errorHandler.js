784f09ba1714750ed6799d5644d03d45e208513d
Zara B <zara.b2000@example.com>
2025-12-29 23:05:33 +0530

    error handlling

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,23 @@
 module.exports = (err, req, res, next) => {
+    if (err.name === "ValidationError") {
+    return res.status(400).json({
+      message: "Validation Error",
+      errors: Object.values(err.errors).map((e) => e.message),
+    });
+  }
+  if (err.name === "CastError") {
+    return res.status(400).json({
+      message: `Invalid ${err.path}: ${err.value}`,
+    });
+  }
+    if (err.code && err.code === 11000) {
+      return res.status(400).json({
+        message: "Duplicate value error",
+        fields: err.keyValue,
+      });
+    }
     res.status(500).json({
         message: err.message || "server Error"
     });
 };
+
